5aa2e5f4bb259fd3f8f4e7bcc3f8c585e635a496
Nicolaj Egelund <nicomanden@gmail.com>
2017-05-03 20:34:03 +0200

    Registered post types and meta boxes

--- admin/WPSliderAdmin.php
+++ admin/WPSliderAdmin.php
@@ -1,5 +1,21 @@
 <?php
 
 class WPSliderAdmin {
-	
+	public function __construct() {
+		add_action('add_meta_boxes', [$this, 'add_meta_boxes']);
+	}
+
+	public function add_meta_boxes() {
+		add_meta_box(
+			'slider_info', 
+			'Slider info', 
+			[$this, 'render_slider_info_meta'], 
+			'slider', 
+			'side'
+		);
+	}
+
+	public function render_slider_info_meta() {
+		require_once '../views/meta-boxes/slider-info.php';
+	}
 }
--- wp-slider.php
+++ wp-slider.php
@@ -1,8 +1,28 @@
 <?php
 
+/**
+ * Plugin Name: WP Slider
+ * Description: Create elegant sliders and ride hippos
+ * Version: 0.0.1
+ */
+
 class WPSlider {
 	public function __construct() {
+		add_action('init', [$this, 'register_post_type']);
 
+		if (is_admin()) {
+			require_once 'admin/WPSliderAdmin.php';
+
+			new WPSliderAdmin;
+		}
+	}
+
+	public function register_post_type() {
+		register_post_type('slider', [
+            'label' => 'Sliders',
+            'public' => true,
+			'supports' => ['title', 'editor']
+        ]);
 	}
 }
 
